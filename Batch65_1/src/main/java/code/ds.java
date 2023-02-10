dc53d5c7840d9e337cbc15c9e45f86913e08401c
Yuhao Fan <yuhao.fan@nyu.edu>
2023-02-10 15:44:25 -0600

    OOP3

--- Batch65_1/src/main/java/code/ds.java
+++ Batch65_1/src/main/java/code/ds.java
@@ -22,13 +22,10 @@ public class ds {
     {
         System.out.println(a);
         System.out.println(b);
-//        int x = 10;
-//        int y = 5;
- 
-        // Code to swap 'x' (1010) and 'y' (0101)
-        a = a ^ b; // x now becomes 15 (1111)
-        b = a ^ b; // y becomes 10 (1010)
-        a = a ^ b; // x becomes 5 (0101)
+        // a^b^b = a
+        a = a ^ b; // 
+        b = a ^ b; // b = a^b^b=a
+        a = a ^ b; // a = a^b^a^b^b=b
  
         System.out.println("After swap: a = "
                            + a + ", b = " + b);
@@ -71,29 +68,24 @@ public class ds {
     }
     void print2Smallest(int arr[])
     {
-        int first, second, arr_size = arr.length;
- 
-        /* There should be atleast two elements */
+        int arr_size = arr.length;
+
         if (arr_size < 2)
         {
             System.out.println(" Invalid Input ");
             return;
         }
  
-        first = second = Integer.MAX_VALUE;
+        int first=Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
         for (int i = 0; i < arr_size ; i ++)
         {
-            /* If current element is smaller than first
-              then update both first and second */
             if (arr[i] < first)
             {
                 second = first;
                 first = arr[i];
             }
- 
-            /* If arr[i] is in between first and second
-               then update second  */
-            else if (arr[i] < second && arr[i] != first)
+            else if (arr[i] < second)
                 second = arr[i];
         }
         if (second == Integer.MAX_VALUE)
--- Batch65_1/src/main/java/code/tester.java
+++ Batch65_1/src/main/java/code/tester.java
@@ -10,8 +10,9 @@ public class tester {
 		d.swap2(100, 0);
 		System.out.println(d.odd(79));
 		d.largest(124.9, 23.5768, 5675.45);
+		d.isvowel('o');
 		d.isvowel('w');
-		d.print2Smallest(new int[] {1234,124124,112,564,76,68});
+		d.print2Smallest(new int[] {1234,124124,76,68,112,564,76,68});
 		System.out.println(d.areAnagram("wadaf", "fadaw"));
 	}
 
